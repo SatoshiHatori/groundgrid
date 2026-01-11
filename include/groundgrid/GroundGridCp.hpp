ac7fb00816913b0de6f16d4354f106ca75affa8e
SatoshiHatori <contactd11332@gmail.com>
2026-01-12 05:31:32 +0900

    rename .h to .hpp

--- include/groundgrid/GroundGridCp.hpp
+++ include/groundgrid/GroundGridCp.hpp
@@ -23,7 +23,7 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISI
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <groundgrid/GroundGrid.h>
+#include <groundgrid/GroundGrid.hpp>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
--- src/GroundGrid.cpp
+++ src/GroundGrid.cpp
@@ -23,7 +23,7 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISI
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <groundgrid/GroundGrid.h>
+#include <groundgrid/GroundGrid.hpp>
 
 #include <chrono>
 
